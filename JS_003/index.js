1269d0fc76afe2078b60aaea0d84b76b13aa4fd5
NdBR <84131697+NdBR@users.noreply.github.com>
2021-06-24 17:06:07 +0200

    Add files via upload

---
+++ JS_003/index.js
@@ -0,0 +1,40 @@
+// deel 1
+
+const age = 19;
+
+if (age >= 18) {
+
+    console.log("you are 18 or older and granted access!! :)")
+}
+else if (age < 18) {
+
+    console.log("you are too youngh and shall not pass!! :(")
+}
+
+// deel 2
+
+const isFemale = false;
+
+if (isFemale) {
+
+    console.log("you may enter for free");
+
+}
+
+else {
+
+    console.log("you have to pay 10 euro's")
+}
+
+// deel 3
+
+const driverStatus = "bob";
+
+if (driverStatus == "bob") {
+
+    console.log("you can drive home")
+
+}
+else {
+    console.log("you connot drive and have to stay for another drink :)")
+}
